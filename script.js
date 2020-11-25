a6a5abe7a6d02e98514681ad6453771056515607
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-11-25 14:47:45 -0600

    add popup menu

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 // Elements
-const lastListElementOfNav = document.querySelector('.sign-in');
+const lastListElementOfNav = document.querySelector('.nav-ul');
 const navLinkListElements = [...document.querySelectorAll('.nav-link')];
+
+// Classes
+class PopupMenu {
+  constructor(title, listItems) {
+    this.title = title;
+    this.listItems = listItems;
+  }
+
+  render() {
+    // Initialize doc frag
+    const docFrag = document.createDocumentFragment();
+    // Create div and append to docFrag
+    const hoverDetailsDiv = document.createElement('div');
+    hoverDetailsDiv.classList.add('hover-details');
+    hoverDetailsDiv.id = 'popup';
+    docFrag.appendChild(hoverDetailsDiv);
+    // Create h2 and ul and append doc Frag
+    const ulElement = document.createElement('ul');
+    const h2Element = document.createElement('h2');
+    h2Element.textContent = this.title;
+    hoverDetailsDiv.appendChild(ulElement);
+    hoverDetailsDiv.appendChild(h2Element);
+    // Create listItems and append to ul
+    this.listItems.forEach((item) => {
+      const li = document.createElement('li');
+      li.textContent = item;
+      ulElement.appendChild(li);
+
+      // Append to DOM
+      lastListElementOfNav.appendChild(docFrag);
+    });
+  }
+}
 
 // Event Listeners
 navLinkListElements.forEach((navLink) => {
@@ -13,9 +46,13 @@ navLinkListElements.forEach((navLink) => {
 // Functions
 
 function showPopupMenu(event) {
-  console.log(event.target);
+  console.log('working');
+  if (event.target.id === 'company-link') {
+    const popupMenu = new PopupMenu('Company', ['About', 'Customers']);
+    popupMenu.render();
+  }
 }
 
-function hidePopupMenu(event) {
-  console.log('hiding');
+function hidePopupMenu() {
+  document.querySelector('#popup').remove();
 }
